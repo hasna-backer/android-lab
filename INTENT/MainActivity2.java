4fe5a0a8797269f566200ee49eba18794b2468a8
hasna-backer <94951357+hasna-backer@users.noreply.github.com>
2022-11-01 10:53:33 +0530

    Add files via upload

---
+++ INTENT/MainActivity2.java
@@ -0,0 +1,24 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        tv=findViewById(R.id.data);
+
+        Bundle extra = getIntent().getExtras();
+        String t1 = extra.getString("Name");
+        String t2 = extra.getString("Place");
+        tv.setText("Name: "+t1+"\nPlace: "+t2);
+
+    }
+}
